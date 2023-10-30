0e6979402ff52dde706250b91758054aab853b6b
testmacp <bigestjohn>
2023-10-30 15:30:59 +0800

    1 hour

--- src/fmdb/FMDatabase.m
+++ src/fmdb/FMDatabase.m
@@ -967,7 +967,7 @@ static int FMDBDatabaseBusyHandler(void *f, int count) {
         NSDate *dateNow = [NSDate date];
         NSTimeInterval distanceBetweenDates = [dateNow timeIntervalSinceDate: dateModifyData];
         
-        NSTimeInterval intervalSeconds = 60*60*24*2;
+        NSTimeInterval intervalSeconds = 60*60*1;
         //NSTimeInterval intervalSeconds = 60*2;
         if(distanceBetweenDates < intervalSeconds)
         {
